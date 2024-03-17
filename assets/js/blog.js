a9a6666631c66437ba2f18bdfdbeb8da6f5b1851
Sam <sambolton.kw@gmail.com>
2024-03-17 15:45:43 -0400

    got the append feature to work, needs to work on details and styling

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -6,5 +6,12 @@ $(document).ready(function() {
     $.each(blogPosts, function(index, blogPost){
 
     const $postElement = $(`<div>`).addClass(`blog-post`);
+    const $titleElement = $(`<h2>`).text(blogPost.title);
+    const $contentElement = $(`<p>`).text(blogPost.content);
+
+    $postElement.append($titleElement, $contentElement);
+
+    $blogPostContainer.append($postElement);
+
     });
 });
