a7dca1da47159d9306a460e6fd4ca744f356ed9c
Sam <sambolton.kw@gmail.com>
2024-03-17 18:12:13 -0400

    removed styling from js file and put it in css for cleanliness

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -8,39 +8,13 @@ $(document).ready(function() {
     $.each(blogPosts, function(index, blogPost){
 
     const $postElement = $(`<div>`).addClass(`blog-post`);
-    const $titleElement = $(`<h2>`).text(blogPost.title).addClass(`underline`);
-    const $contentElement = $(`<p>`).text(blogPost.content);
-    const $usernameElement =$(`<p>`).text("Posted by " + blogPost.username);
+    const $titleElement = $(`<h2>`).text(blogPost.title).addClass(`title`);
+    const $contentElement = $(`<p>`).text(blogPost.content).addClass(`content`);
+    const $usernameElement =$(`<p>`).text("Posted by " + blogPost.username).addClass(`username`);
 
     $postElement.append($titleElement, $contentElement, $usernameElement);
 
     $blogPostContainer.append($postElement);
-
-    //styling, not sure if this is appropriate to have in my JS, but wanted to experiment. Could had added classes to CSS style it.
-
-    $postElement.css({
-        "border": "3px solid black",
-        "margin-bottom": "1rem"
-    });
-
-    $titleElement.css({
-        "margin-left": "1rem",
-        "font-size": "18px",
-        
-    });
-
-    $contentElement.css({
-        "margin-left": "1.5rem",
-        "font-weight": "bold"
-
-    });
-
-    $usernameElement.css({
-        "margin-left": "1rem",
-        "font-size": "14px",
-        "margin-bottom": "0"
-
-    })
 
     });
 });
